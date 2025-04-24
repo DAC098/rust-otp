1267e5070bc2cf3d894ba3d1cfb27c8cc96140e5
dac098 <me@dac098.me>
2025-04-23 21:59:17 -0700

    logic updates
    1. cleaned up logic for totp code verify
    2. modified the totp verify api to pass the secret as an argument and
       remove it from the settings.
    3. TotpSettings implements Default
    4. consolodated the logic into lib
    5. once_off no longer returns a result and the hmac is assumed to always
       be valid when created.

--- benches/gen.rs
+++ benches/gen.rs
@@ -1,29 +1,35 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
-use rust_otp::{Algo, totp};
+use rust_otp::{totp, Algo};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let sha1_secret = b"12345678901234567890";
     let sha256_secret = b"12345678901234567890";
     let sha512_secret = b"12345678901234567890";
 
-    c.bench_function("gen totp SHA1 1_000", |b| b.iter(|| {
-        for _ in 0..1_000 {
-            totp(&Algo::SHA1, sha1_secret, 6, 30, 1234567890);
-        }
-    }));
+    c.bench_function("gen totp SHA1 1_000", |b| {
+        b.iter(|| {
+            for _ in 0..1_000 {
+                totp(&Algo::SHA1, sha1_secret, 6, 30, 1234567890);
+            }
+        })
+    });
 
-    c.bench_function("gen totp sha256 1_000", |b| b.iter(|| {
-        for _ in 0..1_000 {
-            totp(&Algo::SHA256, sha256_secret, 6, 30, 1234567890);
-        }
-    }));
+    c.bench_function("gen totp sha256 1_000", |b| {
+        b.iter(|| {
+            for _ in 0..1_000 {
+                totp(&Algo::SHA256, sha256_secret, 6, 30, 1234567890);
+            }
+        })
+    });
 
-    c.bench_function("gen totp sha512 1_000", |b| b.iter(|| {
-        for _ in 0..1_000 {
-            totp(&Algo::SHA512, sha512_secret, 6, 30, 1234567890);
-        }
-    }));
+    c.bench_function("gen totp sha512 1_000", |b| {
+        b.iter(|| {
+            for _ in 0..1_000 {
+                totp(&Algo::SHA512, sha512_secret, 6, 30, 1234567890);
+            }
+        })
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
--- src/time.rs
+++
@@ -1,9 +0,0 @@
-use std::time::SystemTime;
-
-#[inline]
-pub fn unix_epoch_sec() -> Option<u64> {
-    match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
-        Ok(d) => Some(d.as_secs()),
-        Err(_err) => None,
-    }
-}
